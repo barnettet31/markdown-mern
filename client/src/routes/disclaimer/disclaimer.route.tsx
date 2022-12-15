cf47376835cb254fa8ac1b60eeef8956e74322bc
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-14 20:27:07 -0500

    created non-auth routes

---
+++ client/src/routes/disclaimer/disclaimer.route.tsx
@@ -0,0 +1,5 @@
+ const Disclaimer =()=>{
+    return(<h1>This is my disclaimer</h1>)
+}
+
+export default Disclaimer;
---
+++ client/src/routes/home/home.route.tsx
@@ -0,0 +1,5 @@
+ const HomePage =()=>{
+    return(<h1>This is my landing</h1>)
+}
+
+export default HomePage;
