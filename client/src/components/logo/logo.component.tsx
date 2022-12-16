50482f21308580adff4e52bfa658f8e8099d7977
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-15 21:20:21 -0500

    added hover effect to logo

--- client/src/components/logo/logo.component.tsx
+++ client/src/components/logo/logo.component.tsx
@@ -1,3 +1,3 @@
 import logo from '../../assets/graphics/logo.svg';
 
-export const Logo = ()=> <img src={logo} alt="Markdown"/>
+export const Logo = ()=> <img className="hover:opacity-70 ease-in-out" src={logo} alt="Markdown"/>
