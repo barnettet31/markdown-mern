b12ad5434b82f41a5681d26b9fd57dc1f51da633
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-16 08:54:35 -0500

    fixed underline css rule

--- client/src/routes/home/home.route.tsx
+++ client/src/routes/home/home.route.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
     return (
     <div className="flex flex-col gap-3">
       <AppTypography intent="h2" className="text-xl md:text-2xl lg:text-3xl">Welcome To Mark Down</AppTypography>
-        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">This app is both the completion of the <a className="text-underline font-bold" href="https://www.frontendmentor.io/">Front End Mentor</a> challenge and one of my capstone projects for Promineo Tech Bootcamp.</AppTypography>
+        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">This app is both the completion of the <a className="underline font-bold" href="https://www.frontendmentor.io/">Front End Mentor</a> challenge and one of my capstone projects for Promineo Tech Bootcamp.</AppTypography>
 
     </div>
   );
