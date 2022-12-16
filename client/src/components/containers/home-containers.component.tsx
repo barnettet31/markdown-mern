fbb70743d5afd7b740917b16eac47d04e4e4c3c8
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-15 21:20:47 -0500

    modified home nav

--- client/src/components/containers/home-containers.component.tsx
+++ client/src/components/containers/home-containers.component.tsx
@@ -5,7 +5,7 @@ interface IProps {
 }
 export const HomeContainer = ({ children }: IProps) => {
   return (
-    <main className="dark:bg-primary-black dark:text-tertiary-gray bg-primary-white w-full h-full">
+    <main className="dark:bg-primary-black dark:text-tertiary-gray bg-primary-white w-full h-full px-3 md:px-0">
       <div className="container mx-auto sm:px-6 lg:px-8">{children}</div>
     </main>
   );
--- client/src/components/navbar/homeNavbar.component.tsx
+++ client/src/components/navbar/homeNavbar.component.tsx
@@ -3,23 +3,33 @@ import { Logo } from "../logo/logo.component";
 
 export const HomeNavBar = () => {
   return (
-    <nav className="flex gap-3 bg-tertiary-black py-7 px-5 items-center justify-between">
-      <Link to="/">
-        <Logo />
-      </Link>
-      <div className="self-end flex gap-6">
-        <Link className="text-primary-white hover:opacity-70 ease-in-out" to="/login">
-          Login
+    <nav className="bg-tertiary-black py-4 px-4 md:px-0">
+      <div className="container mx-auto sm:px-6 lg:px-8 flex gap-3 items-center justify-between">
+        <Link to="/">
+          <Logo />
         </Link>
-        <Link className="text-primary-white hover:opacity-70 ease-in-out" to="/signup">
-          SignUp
-        </Link>
-        <Link className="text-primary-white hover:opacity-70 ease-in-out" to="/code">
-          Code
-        </Link>
-        <Link className="text-primary-white hover:opacity-70 ease-in-out" to="/disclaimer">
-          Disclaimer
-        </Link>
+        <div className="self-end flex gap-6">
+          <Link
+            className="text-primary-white hover:opacity-70 ease-in-out"
+            to="/login">
+            Login
+          </Link>
+          <Link
+            className="text-primary-white hover:opacity-70 ease-in-out"
+            to="/signup">
+            SignUp
+          </Link>
+          <Link
+            className="text-primary-white hover:opacity-70 ease-in-out"
+            to="/code">
+            Code
+          </Link>
+          <Link
+            className="text-primary-white hover:opacity-70 ease-in-out"
+            to="/disclaimer">
+            Disclaimer
+          </Link>
+        </div>
       </div>
     </nav>
   );
