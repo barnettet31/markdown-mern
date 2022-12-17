25cc99ad510bd107dca9881091bb35619b816c0a
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-17 11:37:31 -0500

    added page fit

--- client/src/components/containers/home-containers.component.tsx
+++ client/src/components/containers/home-containers.component.tsx
@@ -5,7 +5,7 @@ interface IProps {
 }
 export const HomeContainer = ({ children }: IProps) => {
   return (
-    <main className="dark:bg-primary-black dark:text-tertiary-gray bg-primary-white w-full h-full px-3 md:px-0">
+    <main className="dark:bg-primary-black dark:text-tertiary-gray bg-primary-white px-3 md:px-0 w-full h-full min-h-fit">
       <div className="container mx-auto sm:px-6 lg:px-8">{children}</div>
     </main>
   );
