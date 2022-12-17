dbea2e123b25e2c543996325c1d668c888fad160
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-17 11:37:19 -0500

    added colors to extend config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -10,23 +10,24 @@ module.exports = {
         "default": ["Roboto", "sans-serif"],
         "display": ["Roboto Slab", "sans-serif"],
         "body": ["Roboto Mono", "sans-serif"],
+      },
+      colors:{
+        "primary-black":'#151619',
+        "secondary-black":'#1d1f22',
+        "tertiary-black":'#1d1f22',
+        "secondary-black":'#1d1f22',
+        "primary-gray":'#5a6069',
+        "secondary-gray":'##7c8187',
+        "tertiary-gray":'#c1c4cb',
+        "quaternary-gray":'#e4e4e4',
+        "primary-white":"#f5f5f5",
+        "secondary-white":"#ffffff",
+        "primary-orange":"#e46643",
+        "secondary-orange":"#f39765"
+  
       }
     },
-    colors:{
-      "primary-black":'#151619',
-      "secondary-black":'#1d1f22',
-      "tertiary-black":'#1d1f22',
-      "secondary-black":'#1d1f22',
-      "primary-gray":'#5a6069',
-      "secondary-gray":'##7c8187',
-      "tertiary-gray":'#c1c4cb',
-      "quaternary-gray":'#e4e4e4',
-      "primary-white":"#f5f5f5",
-      "secondary-white":"#ffffff",
-      "primary-orange":"#e46643",
-      "secondary-orange":"#f39765"
-
-    }
+    
   },
   plugins: [],
 }
