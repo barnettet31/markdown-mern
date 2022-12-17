783dde8a20d085179bd7c40e911bef94743671e2
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-17 11:37:43 -0500

    finalized home page

--- client/src/components/typography/app-typography.component.tsx
+++ client/src/components/typography/app-typography.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 interface IAppTypography {
-    intent:'h2'| 'h3' | 'p';
+    intent:'h2'| 'h3' | 'p' ;
     children:React.ReactNode;
     className?:string;
 }
@@ -10,11 +10,11 @@ export const AppTypography =({intent, children, className}:IAppTypography)=>{
     const getClasses = function (intent:string):string{
         switch(intent){
             case 'h2':
-                return 'font-default text-base font-medium text-secondary-gray dark:text-secondary-white '
+                return 'font-default text-base font-medium text-secondary-gray dark:text-secondary-white mt-8 '
             case 'h3':
-                return 'font-default text-sm font-medium text-secondary-gray dark:text-secondary-white '
+                return 'font-default text-sm font-medium text-secondary-gray dark:text-secondary-white mt-8 '
             case 'p':
-                return 'font-default text-xs font-normal text-secondary-gray dark:text-secondary-white '
+                return 'font-default text-xs font-normal text-secondary-gray dark:text-secondary-white mt-8 '
             default:
                 return '';
             }
--- client/src/routes/home/home.route.tsx
+++ client/src/routes/home/home.route.tsx
@@ -1,11 +1,48 @@
+import { Link } from "react-router-dom";
+import App from "../../App";
 import { AppTypography } from "../../components/typography/app-typography.component";
 
 const HomePage = () => {
-    return (
-    <div className="flex flex-col gap-3">
-      <AppTypography intent="h2" className="text-xl md:text-2xl lg:text-3xl">Welcome To Mark Down</AppTypography>
-        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">This app is both the completion of the <a className="underline font-bold" href="https://www.frontendmentor.io/">Front End Mentor</a> challenge and one of my capstone projects for Promineo Tech Bootcamp.</AppTypography>
+  return (
+    <div className="flex flex-col gap-4 w-full text-justify px-4 md:py-8">
+        <AppTypography
+          intent="h2"
+          className="text-2xl md:text-3xl lg:text-4xl underline">
+          Welcome To Mark Down
+        </AppTypography>
+        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">
+          This app is both the completion of the{" "}
+          <a
+            className="underline font-bold"
+            href="https://www.frontendmentor.io/challenges/inbrowser-markdown-editor-r16TrrQX9/hub">
+            Front End Mentor
+          </a>
+          challenge and one of my capstone projects for Promineo Tech Bootcamp.
+        </AppTypography>
+        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">
+          The class requirements for this application are as follows:
+        </AppTypography>
+        <ol className="text-prose font-default text-lg md:text-xl lg:text-2xl list-decimal list-inside">
+          <li>Create a full CRUD application of your choice. </li>
+          <li>When using an existing API, use AJAX to interact with it</li>
+          <li>Use a form to add new entities</li>
+          <li>Build a way for users to update or delete entities</li>
+          <li>Use Bootstrap and CSS to style your project</li>
+        </ol>
+        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">
+          Now technically I didn't actually follow these rules exactly. For
+          example, instead of writing this app using bootstrap for styling, I
+          opted to use tailwind to create my own design system.
+        </AppTypography>
+        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">
+          Additionally, I decided that it would be best to challenge myself
+          further and write my own <span className="text-green-700 font-semibold">node.js</span> backend to handle my
+          CRUD operations server side!
+        </AppTypography>
+        <AppTypography intent="p" className="text-lg md:text-xl lg:text-2xl">The app is built in React with Typescript for type safety and is capable of rendering Markdown added by the user into a preview window in the browser, and saving user data to be updated OR deleted in the future.</AppTypography>
+        <Link to="/code" className="inline-flex w-72 text-center items-center mt-8 rounded-md border border-transparent bg-primary-orange px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-secondary-orange ">Check out the Code Here</Link>
 
+      
     </div>
   );
 };
