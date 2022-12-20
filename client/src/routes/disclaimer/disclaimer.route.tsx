f1dbc8f8ab263e46fcaabaee8e51c16d3581a256
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-12-19 20:31:11 -0500

    added disclaimer

--- client/src/routes/disclaimer/disclaimer.route.tsx
+++ client/src/routes/disclaimer/disclaimer.route.tsx
@@ -1,5 +1,13 @@
+import { AppTypography } from "../../components/typography/app-typography.component";
+
  const Disclaimer =()=>{
-    return(<h1>This is my disclaimer</h1>)
+    return(
+        <div className="flex flex-col gap-4">
+    <AppTypography intent="h2">Demo Application</AppTypography>
+    <AppTypography intent="p">If it isn't already obvious, this application is for demo purposes only</AppTypography>
+    <AppTypography intent="p">I will typically maintain users as long as I can but I will remove them if the cost of this app exceeds what I'm willing to pay.</AppTypography>
+    <AppTypography intent="p">I do this because it's free to use at the moment, mostly because this was built with a design provided by FrontEndMentor and implemented obviously with my own code</AppTypography></div>
+    )
 }
 
 export default Disclaimer;
