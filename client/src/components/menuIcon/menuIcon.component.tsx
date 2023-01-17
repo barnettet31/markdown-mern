9a8cd3ad098a5d5fe06ad0055fe9ff33a322dc07
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-16 21:03:56 -0500

    created component for menu icon

---
+++ client/src/components/menuIcon/menuIcon.component.tsx
@@ -0,0 +1,7 @@
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+export const MenuIcon = () => {
+    return <div className="flex items-center justify-center bg-[#35393F] cursor-pointer flex-auto h-[72px] w-[72px]">
+      <Bars3Icon className="h-[30px] w-[30px] text-white" />
+    </div>;
+}
