b8474502584d0aa5d333876b2ba91d2a7c1cd8b8
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-18 18:46:49 -0500

    updated hover effect for trashcan icon

--- client/src/components/navigation/navigation.component.tsx
+++ client/src/components/navigation/navigation.component.tsx
@@ -28,7 +28,7 @@ export const Navigation = ({
           </div>
         </div>
         <div className="flex items-center gap-6 md:gap-8">
-          <TrashIcon className="text-secondary-gray h-5 w-5 hover:text-primary-white cursor-pointer" />
+          <TrashIcon className="text-secondary-gray h-5 w-5 hover:text-primary-orange cursor-pointer" />
           <button
             type="button"
             className="inline-flex items-center rounded-md border border-transparent bg-primary-orange px-3 md:px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-secondary-orange">
