67d5950064c6e07f53b0fbe45423f2bf2221890b
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-18 18:47:08 -0500

    created container for document elements

--- client/src/components/aside/aside.component.tsx
+++ client/src/components/aside/aside.component.tsx
@@ -1,8 +1,11 @@
-import { PlusIcon } from "@heroicons/react/24/outline";
+import {  DocumentIcon, PlusIcon } from "@heroicons/react/24/outline";
 import styles from "../../layouts/dashboard/userLayout.module.css";
 import { Logo } from "../logo/logo.component";
 import { ThemeSlider } from "../themeSlider/themeSlider.component";
+import { LogoutButton } from "../logoutButton/logoutButton.component";
+
 export const Aside = () => {
+  const logout = () => {console.log('logout')}//TODO implement logout
   return (
     <div
       className={`bg-primary-black flex flex-col justify-between px-6 py-7 ${styles.aside}`}>
@@ -13,15 +16,32 @@ export const Aside = () => {
         </h2>
         <button
           type="button"
-          className="inline-flex items-center rounded-md border border-transparent bg-primary-orange py-2 mt-7 w-full justify-center text-white shadow-sm hover:bg-secondary-orange">
-          <PlusIcon className="h-3 w-3 md:-ml-1 md:mr-1" />
+          className="inline-flex items-center justify-center w-full py-2 text-white border border-transparent rounded-md shadow-sm bg-primary-orange mt-7 hover:bg-secondary-orange">
+          <PlusIcon className="w-3 h-3 md:-ml-1 md:mr-1" />
           <span className="text-sm leading-5 font-default font-regular">
-            Save Changes
+            New Document
           </span>
         </button>
       </div>
-      <div className="flex flex-col gap-7"></div>
-      <ThemeSlider/>
+      <div className="flex flex-col self-start flex-1 py-4 mt-6 overflow-scroll max-h-[500px] md:max-h-[1024px] lg:max-h-[900px] gap-7 scrollbar-hide">
+        {Array(25).fill(1).map((_) => (
+          <div className="flex items-center gap-4 cursor-pointer group">
+            <DocumentIcon className="w-4 h-4 text-white hover:opacity-100" />
+            <div>
+              <p className="hidden text-sm font-light text-secondary-gray md:inline">
+                Document Name
+              </p>
+              <p className="text-sm text-primary-white group-hover:text-primary-orange">
+                welcome.md
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col gap-4">
+        <LogoutButton logout={() => logout()} />
+        <ThemeSlider />
+      </div>
     </div>
   );
 };
