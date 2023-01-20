86b0c577596ad0bdc6bb471c0a676a864bc6b2b7
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-20 15:33:00 -0500

    fixed user document display issue

--- client/src/routes/userDocument/userDocument.route.tsx
+++ client/src/routes/userDocument/userDocument.route.tsx
@@ -10,12 +10,14 @@ import { useQuery } from "react-query";
 import { ErrorPage } from "../../components/errorPage/errorPageComponent";
 export const UserDocument = () => {
     const {id} = useParams();
+    const {setMarkDown} = usePreview()
     if(!id) return <ErrorPage/>
     const {isLoading, data, isError} = useQuery(['document'], ()=>getDocument(id))
     console.log(data);
     const {preview, setPreview} = usePreview();
     const [myMarkdown, setMyMarkdown] = useState<string | undefined>(data?.document.content);
     if(isLoading) return <div>Loading....</div>;
+
     if(data?.document) return <>
       <div
         className={`dark:bg-primary-black flex flex-col min-w-[50vw] bg-secondary-white border-r border-quaternary-gray dark:border-primary-gray ${
@@ -30,10 +32,14 @@ export const UserDocument = () => {
             className="w-4 cursor-pointer md:hidden text-secondary-gray dark:text-tertiary-gray hover:text-primary-orange"
           />
         </div>
-        <CodeEditor markdown={myMarkdown} callback={setMyMarkdown} />
+        <CodeEditor markdown={myMarkdown} callback={(v)=>{
+          setMarkDown(v);
+          setMyMarkdown(v);
+          
+          }} />
       </div>
       <div
-        className={`dark:bg-primary-black  bg-secondary-white border-r border-gray-100  ${styles.preview}`}>
+        className={`min-w-[50vw] dark:bg-primary-black  bg-secondary-white border-r border-gray-100  ${styles.preview}`}>
         <div className="flex justify-between w-full px-4 py-3 dark:bg-secondary-black bg-primary-white">
           <h2 className="text-sm leading-4 text-default dark:text-tertiary-gray text-secondary-gray">
             PREVIEW
