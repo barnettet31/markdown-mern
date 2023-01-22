5882d20cfa28849476c28bb4279d091c60d809ee
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-21 21:14:26 -0500

    added headers to the app.use

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,55 +13,61 @@ const port = 8080;
 init();
 
 app.use(cors({
-  origin: function(origin, callback){
+  origin: function (origin, callback)
+  {
     console.log(origin);
     // allow requests with no origin 
     // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1)
+    {
       var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+        'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
   },
   optionsSuccessStatus: 200,
-  credentials:true
-}))
-app.use(cookieParser())
+  credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 initPassportAndSession(app);
 
 // const connection = mongoose.createConnection(DB, {});
-app.use((req, res, next) => {
+app.use((req, res, next) =>
+{
   console.log("SESSIONS:", req.session);
   console.log("COOKIES:", JSON.stringify(req.cookies));
   next();
 });
 app.use(function (req, res, next)
 {
-  res.header('Access-Control-Allow-Credentials', "true");
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  next();
-});
-//USER ROUTER
-app.use("/api/users", UserRouter);
-app.use("/api/documents", DocumentRouter);
-app.listen(port, () => {
-  console.log(`App started at http://localhost:${port}`);
-});
-
-app.all("*", (req, res, next) => {
-  //console
-  res.status(404).json({
-    status: "fail",
-    message: `Failed to find route ${req.originalUrl}`,
+  
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
   });
-});
+  //USER ROUTER
+  app.use("/api/users", UserRouter);
+  app.use("/api/documents", DocumentRouter);
+  app.listen(port, () =>
+  {
+    console.log(`App started at http://localhost:${port}`);
+  });
+
+  app.all("*", (req, res, next) =>
+  {
+    //console
+    res.status(404).json({
+      status: "fail",
+      message: `Failed to find route ${req.originalUrl}`,
+    });
+  });
 
 //error handler 
 
