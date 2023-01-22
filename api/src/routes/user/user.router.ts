446754129b584b693cd9004cbcf9a147de143c6a
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-21 21:05:48 -0500

    trying something different with passport

--- api/src/routes/user/user.router.ts
+++ api/src/routes/user/user.router.ts
@@ -7,33 +7,10 @@ router.route("/register").post(createUser);
 
 router.get("/me", me);
 
-router.post("/login", (req, res) =>
-{
-    if (!req.body.username || !req.body.password)
-    {
-        return res.status(400).json({ message: 'Please fill out all fields' });
-    }
-    passport.authenticate('local',  function (err, user, info)
-    {
-        if (err) { return res.status(500).json({ message: err }); }
-        else
-        {
-            console.log("user in passport",user);
-            if (!user) {
-                console.log("there isn't a user but here's some info:", info)
-                return res.status(401).json({ message: 'Invalid credentials' });
-            }
-            req.logIn(user,  function (err){ 
-                if (err) { return res.status(500).json({ message: err }); }
-                else
-                {
-                    console.log("user in passport",user);
-                    return res.status(200).json({message:'success'});
-                }
-            })
-        }
-    })(req, res);
-})
+router.post("/login", (req, res) =>passport.authenticate('local'), (req, res) => {
+    res.status(200).json({message:'success'});
+}
+)
 router.post("/logout", function(req,res, next){
     req.logout(function(err){
         if(err) return next(err);
