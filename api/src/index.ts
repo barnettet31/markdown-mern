5ccca9356fc481e330681ef4f9a231930553949f
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-22 12:28:42 -0500

    added correct cors requests

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,22 +13,8 @@ const port = 8080;
 init();
 
 app.use(cors({
-  origin: function (origin, callback)
-  {
-    console.log(origin);
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1)
-    {
-      var msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  optionsSuccessStatus: 200,
-  credentials: true
+  origin:"http://localhost:5173",
+  credentials: true,
 }));
 app.use(cookieParser());
 app.use(express.json());
@@ -39,11 +25,13 @@ initPassportAndSession(app);
 // const connection = mongoose.createConnection(DB, {});
 
 app.use(function (req, res, next)
-{
-    res.header("Access-Control-Allow-Origin", "*");
+{ 
+    console.log(req.body);
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials");
     res.header('Access-Control-Allow-Credentials', "true");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    console.log("headers set")
     next();
   });
   //USER ROUTER
