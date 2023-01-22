96dadfb104eb6266da44a8b9e2ab86d2756d08d4
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-21 19:11:22 -0500

    added log of the uri

--- api/src/middlewares/passport.middleware.ts
+++ api/src/middlewares/passport.middleware.ts
@@ -8,7 +8,7 @@ import config from "../config/config";
 import flash from 'connect-flash';
 import path from "path";
 let uri = config.DATABASE_URL.replace("<PASSWORD>", config.DATABASE_PASSWORD);
-
+console.log(uri);
 
 export default function initPassportAndSession(app: express.Application) {
   app.use(session({
