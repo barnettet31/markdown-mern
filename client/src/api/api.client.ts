64f930a076ccaab1fefb2611809992b66aacf79c
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-23 15:35:42 -0500

    added new api origin to the header

--- client/src/api/api.client.ts
+++ client/src/api/api.client.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://markdown-mern-production.up.railway.app/api',//http://localhost:8080/api
+    baseURL: 'https://api.travreadz.com/api',//http://localhost:8080/api
     headers:{
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'api.travreadz.com',
         'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials',
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
         'Access-Control-Allow-Credentials': 'true'
