ca68a0a7a1e331925aff5497201379a71d9c539c
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-23 12:15:12 -0500

    moved other stuff to trigger rebuild

--- api/src/index.ts
+++ api/src/index.ts
@@ -19,6 +19,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+initPassportAndSession(app);
 app.use(function (req, res, next)
 {
   console.log(req.user);
@@ -30,7 +31,6 @@ app.use(function (req, res, next)
 
   next();
 });
-initPassportAndSession(app);
 
 // const connection = mongoose.createConnection(DB, {});
 
