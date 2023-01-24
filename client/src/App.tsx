8f3aa37717cede6fec69c48c4da4c58275313842
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-24 13:23:44 -0500

    removed session context from root level

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,13 @@ import { ApplicationRouter } from "./routes/router";
 import { ApiClientProvider } from "./api/api.config";
 import { ThemeProvider } from "./context/theme.context";
 import { DefaultError } from "./components/errorPage/defaultError.component";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { SessionContext, SessionContextProvider } from "./context/session.context";
 function App() {
   return (
     <ErrorBoundary errorElement={<DefaultError />}>
       <ApiClientProvider>
         <ThemeProvider>
-          <SessionContextProvider>
             <ApplicationRouter />
-          </SessionContextProvider>
+          
         </ThemeProvider>
       </ApiClientProvider>
     </ErrorBoundary>
