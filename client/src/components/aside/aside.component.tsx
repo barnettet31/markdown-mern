a972bae5936f33a7fa32e4ad5ff55a21c146ab7c
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-24 14:01:25 -0500

    oops broke the nav

--- client/src/components/aside/aside.component.tsx
+++ client/src/components/aside/aside.component.tsx
@@ -34,27 +34,27 @@ export const Aside = ({handleToggle}:IProps) => {
     }
   }
   return (
-    <div className="flex flex-col gap-4">
     <div
       className={`bg-primary-black flex flex-col justify-between px-6 py-7 ${styles.aside}`}>
-      <div className="">
-        <Logo classes="lg:hidden" />
-        <h2 className="text-default text-secondary-gray text-sm leading-4 font-[500] text-left mt-7 lg:mt-0 self-start">
-          My Documents
-        </h2>
-        <button
-          type="button"
-          onClick={createADocument}
-          className="inline-flex items-center justify-center w-full py-2 text-white border border-transparent rounded-md shadow-sm bg-primary-orange mt-7 hover:bg-secondary-orange">
-          <PlusIcon className="w-3 h-3 md:-ml-1 md:mr-1" />
-          <span className="text-sm leading-5 font-default font-regular">
-            New Document
-          </span>
-        </button>
-      </div>
-      <div className="flex flex-col self-start flex-1 py-4 mt-6 overflow-scroll max-h-[500px] md:max-h-[1024px] lg:max-h-[900px] gap-7 scrollbar-hide">
-      <DocumentLinks/>
-      </div>
+      <div className="flex flex-col gap-4">
+        <div className="">
+          <Logo classes="lg:hidden" />
+          <h2 className="text-default text-secondary-gray text-sm leading-4 font-[500] text-left mt-7 lg:mt-0 self-start">
+            My Documents
+          </h2>
+          <button
+            type="button"
+            onClick={createADocument}
+            className="inline-flex items-center justify-center w-full py-2 text-white border border-transparent rounded-md shadow-sm bg-primary-orange mt-7 hover:bg-secondary-orange">
+            <PlusIcon className="w-3 h-3 md:-ml-1 md:mr-1" />
+            <span className="text-sm leading-5 font-default font-regular">
+              New Document
+            </span>
+          </button>
+        </div>
+        <div className="flex flex-col self-start flex-1 py-4 mt-6 overflow-scroll max-h-[500px] md:max-h-[1024px] lg:max-h-[900px] gap-7 scrollbar-hide">
+          <DocumentLinks />
+        </div>
       </div>
       <div className="flex flex-col gap-4">
         <LogoutButton logout={() => logout()} />
