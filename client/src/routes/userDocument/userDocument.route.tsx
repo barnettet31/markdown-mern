30f654b1984a736d74282ffd5458627aa22d9ae5
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2023-01-23 19:38:25 -0500

    changed return statement

--- client/src/routes/userDocument/userDocument.route.tsx
+++ client/src/routes/userDocument/userDocument.route.tsx
@@ -13,16 +13,16 @@ import LoadingIndicator from "../../components/loadingIndicator/loading.componen
 export const UserDocument = () => {
   const [myMarkdown, setMyMarkdown] = useState<string | undefined>("");
   const { id } = useParams();
-  const {updateMarkDown} = useDocumentContext()
+  const { updateMarkDown } = useDocumentContext();
   const { isLoading, data, isError, error, refetch } = useQuery(
     "document",
     () => getDocument(id),
     {
       onSuccess: (data) => {
-        if(data){
-          if(data.document){
+        if (data) {
+          if (data.document) {
             updateMarkDown(data.document.content);
-            setMyMarkdown(data.document.content)
+            setMyMarkdown(data.document.content);
           }
         }
       },
@@ -30,52 +30,50 @@ export const UserDocument = () => {
   );
   const { preview, setPreview } = usePreview();
   if (isLoading) return <LoadingIndicator />;
-
-  if (data?.document)
-    return (
-      <>
-        <div
-          className={`dark:bg-primary-black flex flex-col min-w-[50vw] bg-secondary-white border-r border-quaternary-gray dark:border-primary-gray ${
-            styles.editor
-          } ${preview ? "hidden" : ""}`}>
-          <div className="flex gap-4 justify-between min-w-full px-4 py-3 dark:bg-secondary-black bg-primary-white">
-            <h2 className="text-sm leading-4 text-default dark:text-tertiary-gray text-secondary-gray">
-              MARKDOWN
-            </h2>
-            <EyeIcon
-              onClick={() => setPreview()}
-              className="w-4 cursor-pointer md:hidden text-secondary-gray dark:text-tertiary-gray hover:text-primary-orange"
-            />
-          </div>
-          <CodeEditor
-            markdown={myMarkdown}
-            callback={(v) => {
-              updateMarkDown(v);
-              setMyMarkdown(v);
-            }}
+  if (isError) return <ErrorPage />;
+  return (
+    <>
+      <div
+        className={`dark:bg-primary-black flex flex-col min-w-[50vw] bg-secondary-white border-r border-quaternary-gray dark:border-primary-gray ${
+          styles.editor
+        } ${preview ? "hidden" : ""}`}>
+        <div className="flex gap-4 justify-between min-w-full px-4 py-3 dark:bg-secondary-black bg-primary-white">
+          <h2 className="text-sm leading-4 text-default dark:text-tertiary-gray text-secondary-gray">
+            MARKDOWN
+          </h2>
+          <EyeIcon
+            onClick={() => setPreview()}
+            className="w-4 cursor-pointer md:hidden text-secondary-gray dark:text-tertiary-gray hover:text-primary-orange"
           />
         </div>
-        <div
-          className={`min-w-[50vw] dark:bg-primary-black  bg-secondary-white border-r border-gray-100  ${styles.preview}`}>
-          <div className="flex justify-between w-full px-4 py-3 dark:bg-secondary-black bg-primary-white">
-            <h2 className="text-sm leading-4 text-default dark:text-tertiary-gray text-secondary-gray">
-              PREVIEW
-            </h2>
-            {preview ? (
-              <EyeSlashIcon
-                onClick={() => setPreview()}
-                className="w-4 cursor-pointer hover:text-primary-orange"
-              />
-            ) : (
-              <EyeIcon
-                onClick={() => setPreview()}
-                className="w-4 cursor-pointer hover:text-primary-orange"
-              />
-            )}
-          </div>
-          <MarkDown markdown={myMarkdown} />
+        <CodeEditor
+          markdown={myMarkdown}
+          callback={(v) => {
+            updateMarkDown(v);
+            setMyMarkdown(v);
+          }}
+        />
+      </div>
+      <div
+        className={`min-w-[50vw] dark:bg-primary-black  bg-secondary-white border-r border-gray-100  ${styles.preview}`}>
+        <div className="flex justify-between w-full px-4 py-3 dark:bg-secondary-black bg-primary-white">
+          <h2 className="text-sm leading-4 text-default dark:text-tertiary-gray text-secondary-gray">
+            PREVIEW
+          </h2>
+          {preview ? (
+            <EyeSlashIcon
+              onClick={() => setPreview()}
+              className="w-4 cursor-pointer hover:text-primary-orange"
+            />
+          ) : (
+            <EyeIcon
+              onClick={() => setPreview()}
+              className="w-4 cursor-pointer hover:text-primary-orange"
+            />
+          )}
         </div>
-      </>
-    );
-  if (isError) return <ErrorPage />;
+        <MarkDown markdown={myMarkdown} />
+      </div>
+    </>
+  );
 };
